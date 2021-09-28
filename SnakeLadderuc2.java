50525f0dcf0951022c1392b483e7ed7ee0371364
Smruti <lenkasmruti123@gmail.com>
2021-09-28 12:46:14 +0530

    Added: GenerateNumber

---
+++ SnakeLadderuc2.java
@@ -0,0 +1,14 @@
+package SnakeLadder;
+
+public class SnakeLadderuc2 {
+	public static void main(String[] args) {
+		
+		 
+		 int die = (int)(Math.random()*6) + 1;
+		
+		System.out.println("The dice come up " + die);
+	}
+
+}
+
+
