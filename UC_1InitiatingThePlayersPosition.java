0dba97b5071568a4be3a56d96896b5b19a73d9bd
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-28 13:35:19 +0530

    Update UC_1InitiatingThePlayersPosition.java

--- UC_1InitiatingThePlayersPosition.java
+++ UC_1InitiatingThePlayersPosition.java
@@ -1,11 +1,40 @@
 package SnakeLadder;
 
-public class UC_1InitiatingThePlayersPosition {
+public class SnakeLadder {
 	public static void main(String[] args) {
-		System.out.println("welcome to Snake Ladder");
-		System.out.println("Piayer Start At Position 0 ");
+		System.out.println("Start Snake and Ladder Game");
+		int snake = 1;
+		int ladder = 2;
+		int  PresentStep = 0;
+		while ( PresentStep < 100)
+		{
+		int die = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println("The number on die  " + die);
+		int option = (int) Math.floor(Math.random() * 3);	
+		System.out.println("If you get 0 it is noPlay & 2 it is ladder & 1 it is snake " );
+		System.out.println("You got " + option);
+		if(option == ladder)
+		{
+			  PresentStep += die;
+			if(PresentStep > 100)
+				PresentStep -= die;
+			System.out.println("You are on :" + PresentStep);
+		}
+			else if(option == snake)
+			{
+				  PresentStep -= die;
+				if( PresentStep < 0)
+					 PresentStep = 0;
+				System.out.println("You are on : " + PresentStep);			
+			}
+			else 
+			{
+				System.out.println("You are on :" + PresentStep);
+			}
+		
 	}
 }
 
+}
 
 
