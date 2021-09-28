1c53e6a0b86cc689d0acad0311637aa807f82d7f
Smruti <lenkasmruti123@gmail.com>
2021-09-28 12:03:16 +0530

    Added: UC_1InitiatingThePlayersPosition

---
+++ UC_1InitiatingThePlayersPosition.java
@@ -0,0 +1,11 @@
+package SnakeLadder;
+
+public class UC_1InitiatingThePlayersPosition {
+	public static void main(String[] args) {
+		System.out.println("welcome to Snake Ladder");
+		System.out.println("Piayer Start At Position 0 ");
+	}
+}
+
+
+
