f96bfa496bfd296f884631b6fdf4b06569ec18df
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-29 08:49:02 +0530

    Update UC_1InitiatingThePlayersPosition.java

--- UC_1InitiatingThePlayersPosition.java
+++ UC_1InitiatingThePlayersPosition.java
@@ -8,7 +8,7 @@ public class SnakeLadder {
 		int option = (int) Math.floor(Math.random() * 3);
 		int ladder = 1;
 		int snake = 2 ;
-		int PresentStep = 0;
+		int PresentStep = 4;
 		System.out.println("Present You are on Step:" + PresentStep);
 		System.out.println("If you get option as 0 it is noPlay,1 its ladder,2 its snake " );
 		System.out.println("You got option" + option);
